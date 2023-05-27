34e39e56d34dd7d1181b43cc9c929801d63fee73
deajeany16 <cydjeany1601@gmail.com>
2023-05-27 20:20:14 +0700

    Kelola Gaji

--- index.php
+++ index.php
@@ -21,7 +21,39 @@ $gaaji = query("SELECT karyawan_id, nama, nama_golongan, tanggal, potongan, juml
 <body>
     <div class="container">
         
-        <nav class="navbar navbar-default">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a href="#" class="navbar-brand">----</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+            <a class="nav-link text-white bg-primary rounded " href="index.php">Daftar Gaji Karyawan</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link text-dark " href="tampil_gaji.php">Data Gaji</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link text-dark" href="tampil_golongan.php">Data Golongan</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link text-dark " href="tampil_karyawan.php">Data Karyawan</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link text-dark " href="tampil_cuti.php">Data Cuti</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link text-dark " href="tampil_lembur.php">Data Lembur</a>
+            </li>
+        </ul>
+        </div>
+    </nav>
+
+
+    <!--    <nav class="navbar navbar-default">
             <div class="container-fluid">
                 <div class="navbar-header">
                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" 
@@ -46,6 +78,8 @@ $gaaji = query("SELECT karyawan_id, nama, nama_golongan, tanggal, potongan, juml
 		        </div>
             </div>
         </nav>
+-->
+
         <div class="text-center">
             <br>
             <h1>Daftar Gaji Karyawan</h1>
@@ -82,4 +116,14 @@ $gaaji = query("SELECT karyawan_id, nama, nama_golongan, tanggal, potongan, juml
                 </table>
             </div>
 </body>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
 </html>
